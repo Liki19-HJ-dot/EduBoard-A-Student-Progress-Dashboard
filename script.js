8c171c626daa73d46909ff1d5279914d3432b1cb
Likhith H J <19likhith@gmail.com>
2025-07-09 23:18:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Sample interactivity for EduBoard Dashboard
+
+// Display a message when notification bell is clicked
+document.getElementById('notify-icon').addEventListener('click', () => {
+  alert('You have no new notifications.');
+});
+
+// Future: You can dynamically update student stats like GPA, Attendance etc.
+// Example:
+// document.getElementById('gpa').innerText = "9.0";
